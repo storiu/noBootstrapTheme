759d7e439f35f62f70e90303f7e5eaa10eece0ac
RedCarrot0803 <redcarrot0803@gmail.com>
2021-05-04 08:06:35 -0700

    product list add, and show product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
+    ProductListComponent,
   ],
   bootstrap: [
     AppComponent
