e80084e58dbf6dfc9c5be758f5f482b95817888a
RedCarrot0803 <redcarrot0803@gmail.com>
2021-05-05 14:38:33 -0700

    product-detail add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
+    ProductDetailsComponent,
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    // first get product id
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number( routeParams.get('productId') );
+    console.log( productIdFromRoute );
+
+    // find correct product in products
+    this.product = products.find( product => product.id === productIdFromRoute );
+  }
+
+}
